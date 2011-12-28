259a73bda7b687cb923df9fdfebbc6285b31e5ee
Ivan <axxiej@example.com>
2011-12-28 10:03:30 +0000

    Added service Uninstall and main service function
    --HG--
    extra : convert_revision : svn%3A3132c696-2579-4545-bfe7-e0852c457502/trunk%4022

--- MImpPro/MImpSrv/MImpSrv.cpp
+++ MImpPro/MImpSrv/MImpSrv.cpp
@@ -4,9 +4,12 @@
 #include "stdafx.h"
 
 #define MIMP_SERVICE_NAME           L"MImpSrv"
-#define MIMP_SERVICE_DISPLAY_NAME   L"MouseImp elevated launcher service"
+#define MIMP_SERVICE_DISPLAY_NAME   L"MouseImp elevated launcher"
 #define MIMP_SERVICE_DESCRIPTION    L"Launches elevated MouseImp instance in each session on request. Elevation is required by MouseImp hooking in order to provide its functionality inside elevated processes"
 
+#define MIMP_STOP_TIMEOUT           30000 // 30 seconds
+
+#define MIMP_ERROR_REBOOT_REQUIRED  -42
 
 class CScopedServiceHandle
 {
@@ -229,10 +232,8 @@ DWORD InstallService()
             MIMP_SERVICE_DISPLAY_NAME))
         {
             printf("Can't change service config, please reboot and try again");
-
             return GetLastError();
         }
-
     }
 
     SERVICE_DESCRIPTION svcDescription;
@@ -248,10 +249,180 @@ DWORD InstallService()
     return ERROR_SUCCESS;
 }
 
+DWORD UninstallService(bool *rebootRequired)
+{
+    *rebootRequired = false;
+
+    CScopedServiceHandle scmHandle = OpenSCManager(
+        NULL,               // machine (NULL == local)
+        NULL,               // database (NULL == default)
+        SC_MANAGER_CONNECT  // access required
+        );
+
+    if (!scmHandle)
+    {
+        printf("Cannot open SCM to remove service!");
+        return GetLastError(); 
+    }
+
+    CScopedServiceHandle serviceHandle = OpenService(
+        scmHandle, 
+        MIMP_SERVICE_NAME,
+        DELETE | SERVICE_STOP | SERVICE_QUERY_STATUS);
+
+    if (!serviceHandle)
+    {
+        printf("Cannot open service");
+        return GetLastError(); 
+    }
+
+    SERVICE_STATUS serviceStatus;
+
+    // try to stop the service
+    ControlService(serviceHandle, SERVICE_CONTROL_STOP, &serviceStatus);
+
+    DWORD startTime = GetTickCount();
+
+    while (serviceStatus.dwCurrentState != SERVICE_STOPPED)
+    {
+        DWORD dwBytesNeeded;
+
+        Sleep(serviceStatus.dwWaitHint);
+        
+        if (!QueryServiceStatusEx( 
+            serviceHandle, 
+            SC_STATUS_PROCESS_INFO,
+            (LPBYTE)&serviceStatus, 
+            sizeof(SERVICE_STATUS_PROCESS),
+            &dwBytesNeeded))
+        {
+            printf("QueryServiceStatusEx failed (%d)\n", GetLastError());
+            return GetLastError();
+        }
+
+        if (serviceStatus.dwCurrentState == SERVICE_STOPPED)
+        {
+            break;
+        }
+
+        if (GetTickCount() - startTime > MIMP_STOP_TIMEOUT)
+        {
+            printf("Wait timed out\n");
+            break;
+        }
+    }
+
+    if (DeleteService(serviceHandle))
+    {
+        return ERROR_SUCCESS;
+    }
+
+    if (GetLastError() == ERROR_SERVICE_MARKED_FOR_DELETE)
+    {
+        *rebootRequired = true;
+        return ERROR_SUCCESS;
+    }
+
+    return GetLastError();
+}
+
+bool g_serviceMustExit = false;
+SERVICE_STATUS_HANDLE g_serviceStatusHandle;
+
+DWORD g_dwCheckPoint = 1;
+
+void ReportStatusToSCM(
+    DWORD dwCurrentState)
+{
+    SERVICE_STATUS serviceStatus;
+    serviceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS; 
+    serviceStatus.dwCurrentState = dwCurrentState;
+    serviceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+    serviceStatus.dwWin32ExitCode = NO_ERROR;
+    serviceStatus.dwServiceSpecificExitCode = 0; 
+    serviceStatus.dwCheckPoint = g_dwCheckPoint;
+    serviceStatus.dwWaitHint = 2000;
+
+    g_dwCheckPoint++;
+
+    SetServiceStatus(g_serviceStatusHandle, &serviceStatus);
+}
+
+DWORD WINAPI ServiceCtrlHandler(
+    DWORD    dwControl,
+    DWORD    dwEventType,
+    LPVOID   lpEventData,
+    LPVOID   lpContext)
+{
+    if (dwControl == SERVICE_CONTROL_STOP)
+    {
+        ReportStatusToSCM(SERVICE_STOP_PENDING);
+        g_serviceMustExit = true;
+    }
+    return NO_ERROR;
+}
+
+
+void ServiceMain(
+    DWORD dwArgc,
+    LPTSTR *lpszArgv)
+{
+    g_serviceStatusHandle = RegisterServiceCtrlHandlerEx( 
+        MIMP_SERVICE_NAME,
+        &ServiceCtrlHandler,
+        NULL);
+        
+    ReportStatusToSCM(SERVICE_START_PENDING);
+
+    ReportStatusToSCM(SERVICE_RUNNING);
+
+    while (!g_serviceMustExit)
+    {
+        Sleep(1000);
+    }
+
+    ReportStatusToSCM(SERVICE_STOPPED);
+}
+
+DWORD ExecuteAsService() 
+{
+    SERVICE_TABLE_ENTRY dispatchTable[] =
+    {
+        { (LPWSTR)(LPCWSTR)MIMP_SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain},
+        { NULL, NULL}
+    };
+
+    if (!StartServiceCtrlDispatcher(dispatchTable))
+    {
+        return GetLastError();
+    }
+
+    return ERROR_SUCCESS;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    InstallService();
+    if (argc < 2)
+    {
+        return ExecuteAsService();
+    }
+
+    if (!wcscmp(argv[1], L"--install"))
+    {
+        return InstallService();
+    }
+    else if (!wcscmp(argv[1], L"--uninstall"))
+    {
+        bool rebootRequired;
+        DWORD lastError = UninstallService(&rebootRequired);
+        if (rebootRequired)
+        {
+            return MIMP_ERROR_REBOOT_REQUIRED;
+        }
+
+        return lastError;
+    }
+
 	return 0;
 }
 
